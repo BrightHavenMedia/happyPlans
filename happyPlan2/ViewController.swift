95b24cda065a5e58ba7f783138299b08b58f16a0
jamesjens <jensen.jedward@gmail.com>
2017-06-30 14:56:32 -0600

    Initial Commit

---
+++ happyPlan2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  happyPlan2
+//
+//  Created by James Jensen on 6/30/17.
+//  Copyright © 2017 James Jensen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
